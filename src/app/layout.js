96274b65c11d2c16a6437157dfec106f46a2be05
Rohberto <oluwaseunrobert44@gmail.com>
2025-12-08 16:16:24 +0100

    dormai completion

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins, Montserrat, Bebas_Neue } from "next/font/google";
 import "./globals.css";
 import WhatsAppButton from "./utils/WhatsappButton";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
+const poppins = Poppins({ variable: "--font-poppins", subsets: ["latin"], weight: ["400", "700"] });
+const montserrat = Montserrat({ variable: "--font-montserrat", subsets: ["latin"], weight: ["400", "700"] });
+const bebas = Bebas_Neue({ variable: "--font-bebas", subsets: ["latin"], weight: ["400"] });
 
 export const metadata = {
   title: "Dormai Signs - Leading Signage Company in Lagos",
@@ -23,7 +19,7 @@ export const metadata = {
     siteName: "Dormai Signs",
     images: [
       {
-        url: "https://dormaisignsltd.com/Images/card-logo.png",
+        url: "https://dormaisignsltd.com/favicon.png",
         width: 1200,
         height: 630,
         alt: "Dormai Signs Logo",
@@ -38,14 +34,14 @@ export const metadata = {
     description: "Dormai Signs Limited offers premium signage and branding solutions in Lagos, Nigeria.",
     site: "@dormaisigns",
     creator: "@dormaisigns",
-    images: ["https://dormaisignsltd.com/Images/card-logo.png"],
+    images: ["https://dormaisignsltd.com/favicon.png"],
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} ${montserrat.variable} ${bebas.variable}`}>
         {children}
         <WhatsAppButton/>
       </body>
